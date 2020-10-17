7d8c53418079f034877afd81e47dda350bb2549f
ndelvalle <nicolas.delvalle@gmail.com>
2020-10-17 00:56:33 -0300

    Patch to version 0.2.4

--- Formula/nakatoshi.rb
+++ Formula/nakatoshi.rb
@@ -1,9 +1,9 @@
 class Nakatoshi < Formula
   desc "Bitcoin vanity address generator"
   homepage "https://github.com/ndelvalle/nakatoshi"
-  url "https://github.com/ndelvalle/nakatoshi/releases/download/v0.2.3/nakatoshi-macos-amd64.tar.gz"
-  sha256 "1ceebc7e312901dc03f80ffb7564000b474b6451f98bc1c557fe4789da508f6a"
-  version "0.2.3"
+  url "https://github.com/ndelvalle/nakatoshi/releases/download/v0.2.4/nakatoshi-macos-amd64.tar.gz"
+  sha256 "f9d2733d4926daaa1096765c353f71b9132a45162d090778be480bd0724fb54b"
+  version "0.2.4"
 
   def install
     bin.install "nakatoshi"
